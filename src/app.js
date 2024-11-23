31e472d20dd59e323ee5e907817c8affc54d89b3
Tanmay Shende <tanmayshende007@gmail.com>
2024-11-23 13:02:23 +0530

    User Model and instance of the model is created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-const express = require('express');
-require('dotenv').config();
-require('./config/database')
+const express = require("express");
+require("dotenv").config();
+const connectDB = require("./config/database");
+const User = require('./models/user')
 
 const app = express();
-const PORT = process.env.PORT || 1111
+const PORT = process.env.PORT || 1111;
 
+app.post('/signup', (req, res) => {
 
-app.listen(PORT,() => {
-    console.log(`Server is up on port ${PORT}`);
+    // Creation of the new instance of the User Model
+    const user = new User({
+        firstName: 'Manal',
+        lastName: 'Kamble',
+        emailId: 'manal@kamble.com',
+        password: 'manal@123',
+        age: 23,
+        gender: 'Male'
+    })
+    user.save();
+    res.json({
+        status: true,
+        message: 'User added Successfully!!'
+    });
+    
 })
+
+connectDB()
+  .then(() => {
+    console.log("Database connection is established successfully!!");
+
+    app.listen(PORT, () => {
+      console.log(`Server is up on port ${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Database connot be connected..");
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,5 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI)
 }
 
-connectDB()
-.then(() => {
-    console.log('connection is established successfully!!');
-})
-.catch(() => {
-    console.log('Database connot be connected..');
-})
+module.exports = connectDB;
+
