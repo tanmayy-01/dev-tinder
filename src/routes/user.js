259f47b9e41ed47d024ea3694f1f056a254e8a47
Tanmay Shende <tanmayshende007@gmail.com>
2024-12-05 23:57:36 +0530

    limitation is added for limit query

--- src/routes/user.js
+++ src/routes/user.js
@@ -57,7 +57,8 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 userRouter.get('/user/feed', userAuth, async (req, res) => {
     try {
         const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 10;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit; 
         const skip = (page - 1) * limit;
         const connections = await ConnectionRequest.find({
             $or: [
